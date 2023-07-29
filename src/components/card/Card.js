760150a10bfc8b8be2866bd4483bcf43a309d1ab
Érika Laíse <126110376+ErikaLaise@users.noreply.github.com>
2023-07-29 12:26:03 -0300

    Projeto React SWAPI

---
+++ src/components/card/Card.js
@@ -0,0 +1,49 @@
+import "./Card.css";
+
+function Card({
+  foto,
+  nomePlaneta,
+  clima,
+  gravidade,
+  populacao,
+  diametro,
+  superficieAquosa,
+}) {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="cardFront">
+          
+          <h1>Planeta: {nomePlaneta}</h1>
+          <img className="imagem" src={foto} alt="foto do planeta" />
+        </div>
+
+        <div className="cardBack">
+          <h1>Sobre este planeta:</h1>
+
+          <p>
+            <b>Clima:</b>
+            {clima}
+          </p>
+          <p>
+            <b>Gravidade:</b>
+            {gravidade}
+          </p>
+          <p>
+            <b>População:</b>
+            {populacao}
+          </p>
+          <p>
+            <b>Diâmetro:</b>
+            {diametro}
+          </p>
+          <p>
+            <b>Superfície Aquosa:</b>
+            {superficieAquosa}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Card;
---
+++ src/pages/Home.js
@@ -0,0 +1,65 @@
+import { Container, PlanetList, Planet } from "./home.css";
+import { useState, useEffect } from "react";
+import "./home.css";
+import Card from "../components/card/Card";
+
+function Home() {
+  const [planets, setPlanets] = useState([]);
+
+  const imagesPlanets = [
+    "/planetas/TATOOINE1.png",
+    "/planetas/ALDERAAN2.png",
+    "/planetas/YAVIN_IV3.png",
+    "/planetas/HOTH4.png",
+    "/planetas/DAGOBAH5.png",
+    "/planetas/BESPIN6.png",
+    "/planetas/ENDOR7.png",
+    "/planetas/NABOO8.png",
+    "/planetas/CORUSCANT9.png",
+    "/planetas/KAMINO10.png",
+  ];
+
+  async function fetchData() {
+    const response = await fetch(
+      "https://swapi.dev/api/planets/",
+
+      { method: "GET" }
+    );
+    const data = await response.json();
+    setPlanets(data.results);
+    console.log(data.results);
+  }
+  fetchData();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>    
+      <h1>Planetas Star Wars</h1>
+
+      <section className="secaoCards">
+        {planets.map((planet, index) => (
+          <Card
+            key={index}
+            foto={imagesPlanets[index]}
+            nomePlaneta={planet.name}
+            clima={planet.climate}
+            gravidade={planet.gravity}
+            populacao={planet.population}
+            diametro={planet.diameter}
+            superficieAquosa={planet.surface_water} />
+        ))}
+      </section>
+    
+    
+    <footer>
+        <p>Todos os dados foram obtidos do <a className="link" href="https://swapi.dev/api/planets">SWAPI</a></p>
+
+        <p>Desenvolvido por <a className= "link" href="https://github.com/ErikaLaise/"> ® Érika Laíse - 2023</a></p>
+      </footer></>
+  );
+}
+
+export default Home;
